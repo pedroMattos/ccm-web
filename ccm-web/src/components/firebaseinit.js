a55aa0f56e1adf583aff4bd4d541f8f7852c84cc
PedroM <pedro@dnadevendas.com.br>
2020-01-06 16:04:43 -0300

    firebase config

---
+++ ccm-web/src/components/firebaseinit.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// exporta o metodo de autenticação e o banco de dados
+export default firebaseApp.auth() && firebaseApp.firestore();
