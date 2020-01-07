9fde6647542d5fb263269c6722a6aa684efdf5db
PedroM <pedro@dnadevendas.com.br>
2020-01-07 14:21:25 -0300

    add new route

--- ccm-web/src/router/index.js
+++ ccm-web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Dashboard from '@/components/Dashboard';
+import NewEntry from '@/components/NewEntry';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Dashboard,
+    },
+    {
+      path: '/new-entry',
+      name: 'NewEntry',
+      component: NewEntry,
     },
   ],
 });
