435ed7ffe783a07ffcb811b8fd1e4391b659fb0a
PedroM <pedro@dnadevendas.com.br>
2020-01-07 14:20:55 -0300

    ADD: new component new entry

---
+++ ccm-web/src/components/NewEntry.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="main-new-entry">
+      <h1>Nova entrada <span>Embryo version</span></h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'newEntry',
+};
+</script>
+
+<style>
+  #main-new-entry {
+    padding-top: 20px;
+    padding-left: 20px;
+  }
+  #main-new-entry h1{
+    font-size: 32px;
+  }
+    #main-new-entry h1 span{
+      background-color: green;
+      padding: 2px;
+      border-radius: 5px;
+      color: white;
+      font-size: 12px;
+  }
+  .quadros {
+    position: relative;
+  }
+</style>
