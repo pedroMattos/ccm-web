3b967fff918ed6cd1d7153e1e347500940a2041e
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-07 22:38:55 -0300

    add form new entry

---
+++ ccm-web/src/components/forms/newEntryForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Tombo" id="tombo" v-model="tombo" name="tombo" type="text">
+        </div>
+      </div>
+    </form>
+</template>
+
+<script>
+export default {
+  name: 'form-new-entry',
+  data() {
+    return {
+      tombo: '',
+    };
+  },
+};
+</script>
