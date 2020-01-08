ca3a04ee23be8f54247fbe67723fd008fef3aad3
PedroM <pedro@dnadevendas.com.br>
2020-01-08 16:29:29 -0300

    page my entries

---
+++ ccm-web/src/components/MyEntries.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="main-page col m9" id="main-my-entry">
+      <router-link :to="{ name: 'Home' }"><back/></router-link>
+      <h1>Minhas entradas</h1>
+      <div class="main-page row">
+        <myentry/>
+      </div>
+    </div>
+</template>
+
+<script>
+import back from './svg-components/back';
+import myentry from './quadros/myentry';
+
+export default {
+  name: 'my-entry',
+  components: {
+    back,
+    myentry,
+  },
+};
+</script>
