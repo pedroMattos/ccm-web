2ef845887f960801a53d3f055cbe19d0413de603
PedroM <pedro@dnadevendas.com.br>
2020-01-08 16:28:14 -0300

    add qr code generator

---
+++ ccm-web/src/components/QR-code-generator.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main-code">
+      <img :src="url" alt="teste">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'QRcode',
+  data() {
+    return {
+      dados: [
+        {
+          tombo: '54654646546',
+          situation: '',
+          modelo: 'optiplex 3010',
+          issue: 'Fonte',
+          date: '21/10/2019',
+          uid: '18321987329179',
+          name: 'Pedro',
+        },
+      ],
+      url: '',
+    };
+  },
+};
+</script>
