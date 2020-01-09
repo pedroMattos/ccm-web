24c8cbe20f6e9bda02be7bd42f7dc7e18864d683
PedroM <pedro@dnadevendas.com.br>
2020-01-09 15:09:46 -0300

    add form login

---
+++ ccm-web/src/components/forms/formLogin.vue
@@ -0,0 +1,21 @@
+<template>
+  <form id="main-form-login">
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="password" type="password" class="validate">
+        <label for="password">Password</label>
+      </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+  </form>
+</template>
+<script>
+export default {
+  name: 'formLogin',
+}
+</script>
