d425baa33f59e127ffbcce1562eb467e6badbff8
PedroM <pedro@dnadevendas.com.br>
2020-01-09 15:09:03 -0300

    add page login

---
+++ ccm-web/src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="main-login"><formLogin/></div>
+</template>
+
+<script>
+import formLogin from './forms/formLogin';
+
+export default {
+  name: 'Login',
+  components: {
+    formLogin,
+  },
+};
+</script>
