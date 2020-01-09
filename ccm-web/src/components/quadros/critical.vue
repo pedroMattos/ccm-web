33f8fed833f900ba104c5471d6e423f3d87a4fda
PedroM <pedro@dnadevendas.com.br>
2020-01-09 15:10:32 -0300

    add query

--- ccm-web/src/components/quadros/critical.vue
+++ ccm-web/src/components/quadros/critical.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dash-block" id="main-crit-pc">
+    <div :class="[name, 'dash-block']">
       <h2>Estado crítico (urgência)</h2>
       <p>Contagem: <span>{{ count }}</span></p>
       <p class="center">Ver</p>
@@ -7,18 +7,34 @@
 </template>
 
 <script>
+import bd from '../firebaseinit';
+
 export default {
   name: 'critical',
   data() {
     return {
-      count: '20',
+      count: '',
+      name: 'estado-critico',
     };
+  },
+  created() {
+    var context = this;
+    var docRef = bd.collection("Máquinas");
+    docRef.get().then(function(doc) {
+      var i = 0;
+      doc.forEach(doc => {
+        if(doc.data().Estado == context.name) {
+          i ++;
+          context.count = i;
+        }
+      });
+    });
   },
 };
 </script>
 
 <style>
-  #main-crit-pc {
+  #main-crit-pc, .estado-critico {
     background-color: #D9042B;
   }
 </style>
