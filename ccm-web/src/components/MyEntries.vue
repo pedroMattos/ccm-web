623d9c0444ff9441f64d4d15379ffae21b2e43e9
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-08 22:32:42 -0300

    add blockquote

--- ccm-web/src/components/MyEntries.vue
+++ ccm-web/src/components/MyEntries.vue
@@ -2,6 +2,10 @@
     <div class="main-page col m9" id="main-my-entry">
       <router-link :to="{ name: 'Home' }"><back/></router-link>
       <h1>Minhas entradas</h1>
+      <blockquote class="tip">
+        <h2 class="grey-text lighten-2"
+        id="sub-t-page">Aqui você pode ver e atualizar todas as suas entradas</h2>
+      </blockquote>
       <div class="main-page row">
         <myentry/>
       </div>
@@ -20,3 +24,12 @@ export default {
   },
 };
 </script>
+
+<style>
+  #sub-t-page {
+    font-size: 24px;
+  }
+  blockquote.tip {
+    border-left: solid #024873 5px;
+  }
+</style>
