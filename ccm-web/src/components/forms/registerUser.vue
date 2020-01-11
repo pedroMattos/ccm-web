6928e1b1a1e3909ac517598aea0d350b5b55dee5
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-11 02:35:06 -0300

    add new fields in register user

--- ccm-web/src/components/forms/registerUser.vue
+++ ccm-web/src/components/forms/registerUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <form method="post" @submit.prevent="register()" class="z-depth-2" id="main-form-register">
+  <form method="post" @submit.prevent="register()" class="col m6 tt" id="main-form-register">
     <div class="row">
       <div class="input-field col s12">
         <input v-model="email" id="email" type="email" class="validate">
@@ -11,15 +11,84 @@
         <input v-model="password" id="password" type="password" class="validate">
         <label for="password">Senha</label>
       </div>
-      <input type="submit" value="Entrar" class="btn col m12">
-      <span class="center" v-if="err != ''">{{ err }}</span>
-      <p v-if="err != ''">Entre em contato com um administrador do sistema.</p>
+      <div class="input-field col s12">
+        <input v-model="name" id="nome" type="text" class="validate">
+        <label for="password">Nome</label>
+      </div>
+      <p>Nivel</p>
+      <p>
+        <label>
+          <input name="admin" value="Admin" v-model="nivel" type="radio" />
+          <span>Administrador</span>
+        </label>
+      </p>
+      <p>
+        <label>
+          <input name="user" value="User" v-model="nivel" type="radio" />
+          <span>Usuário Padrão</span>
+        </label>
+      </p>
+      <p>Permissões</p>
+      <p>
+        <label>
+          <input name="master" value="Master" v-model="permiss" type="radio" />
+          <span>Master</span>
+        </label>
+      </p>
+      <p>
+        <label>
+          <input name="w/c" value="Write/Create" v-model="permiss" type="radio" />
+          <span>Ler/Criar</span>
+        </label>
+      </p>
+      <input type="submit" value="Criar usuário" class="btn col m12">
     </div>
   </form>
 </template>
 
 <script>
+import auth from '../firebaseinit';
+
 export default {
   name: 'register',
+  data() {
+    return {
+      password: '',
+      email: '',
+      uid: '',
+      nivel: '',
+      name: '',
+      error: '',
+      permiss: '',
+    };
+  },
+  methods: {
+    register() {
+      auth.app.auth().createUserWithEmailAndPassword(this.email, this.password).then((cred) => {
+        const ref = auth.collection('Controller');
+        const id = cred.user.uid;
+        const payload = {
+          Uid: cred.user.uid,
+          Nome: this.name,
+          Nivel: this.nivel,
+          Permiss: this.permiss,
+        };
+        ref.doc(id).set(payload, (error) => {
+          if (error) {
+            this.error = error;
+          } else {
+            this.$router.push({ name: 'Login' });
+            location.reload();
+          }
+        });
+      });
+    },
+  },
 };
 </script>
+
+<style>
+  #main-form-register {
+    padding: 20px;
+  }
+</style>
