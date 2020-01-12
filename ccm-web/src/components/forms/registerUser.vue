42680de402510f8d6719f75627b09dbc38a06d06
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-11 23:07:48 -0300

    change radio button for select

--- ccm-web/src/components/forms/registerUser.vue
+++ ccm-web/src/components/forms/registerUser.vue
@@ -1,49 +1,55 @@
 <template>
-  <form method="post" @submit.prevent="register()" class="col m6 tt" id="main-form-register">
-    <div class="row">
-      <div class="input-field col s12">
-        <input v-model="email" id="email" type="email" class="validate">
-        <label for="email">Email</label>
+  <div class="row">
+    <form method="post" @submit.prevent="register()" class="col m6" id="main-form-register">
+      <div class="row">
+        <div class="input-field col s12">
+          <input v-model="email" id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="input-field col s12">
-        <input v-model="password" id="password" type="password" class="validate">
-        <label for="password">Senha</label>
+      <div class="row">
+        <div class="input-field col s12">
+          <input v-model="password" id="password" type="password" class="validate">
+          <label for="password">Senha</label>
+        </div>
+        <div class="input-field col s12">
+          <input v-model="name" id="nome" type="text" class="validate">
+          <label for="password">Nome</label>
+        </div>
+        <div class="input-field col s12">
+          <select v-model="nivel">
+            <option value="no selected" disabled>Escolha</option>
+            <option value="Admin">Administrador</option>
+            <option value="User">Usuário Padrão</option>
+          </select>
+          <label>Nivel</label>
+        </div>
+        <div class="input-field col s12">
+          <select v-model="nivel">
+            <option value="no selected" disabled>Escolha</option>
+            <option value="Write/Create">Ler/Criar</option>
+            <option value="Master">Todas</option>
+          </select>
+          <label>Permissões</label>
+        </div>
+        <input type="submit" value="Criar usuário" class="btn col m12">
       </div>
-      <div class="input-field col s12">
-        <input v-model="name" id="nome" type="text" class="validate">
-        <label for="password">Nome</label>
-      </div>
-      <p>Nivel</p>
-      <p>
-        <label>
-          <input name="admin" value="Admin" v-model="nivel" type="radio" />
-          <span>Administrador</span>
-        </label>
-      </p>
-      <p>
-        <label>
-          <input name="user" value="User" v-model="nivel" type="radio" />
-          <span>Usuário Padrão</span>
-        </label>
-      </p>
-      <p>Permissões</p>
-      <p>
-        <label>
-          <input name="master" value="Master" v-model="permiss" type="radio" />
-          <span>Master</span>
-        </label>
-      </p>
-      <p>
-        <label>
-          <input name="w/c" value="Write/Create" v-model="permiss" type="radio" />
-          <span>Ler/Criar</span>
-        </label>
-      </p>
-      <input type="submit" value="Criar usuário" class="btn col m12">
-    </div>
-  </form>
+    </form>
+    <blockquote v-if="nivel == 'Admin'" class="alert col m6 grey-text">
+      <h3>Usuário Administrador:</h3>
+      <p>Tem permissões de criar usuários</p>
+      <p>Editar todas as entradas</p>
+      <p>Promover usuários</p>
+      <p>Excluir entradas</p>
+      <p>Criar e ler entradas</p>
+      <p>Geralmente tem acesso ao banco de dados</p>
+    </blockquote>
+    <blockquote v-if="nivel == 'User'" class="alert col m6 grey-text">
+      <h3>Usuário Padrão:</h3>
+      <p>Edita somente suas entradas</p>
+      <p>Cria e lê entradas</p>
+    </blockquote>
+  </div>
 </template>
 
 <script>
@@ -61,6 +67,10 @@ export default {
       error: '',
       permiss: '',
     };
+  },
+  created() {
+    // eslint-disable-next-line no-undef
+    $(document).ready(() => { $('select').formSelect(); });
   },
   methods: {
     register() {
@@ -90,5 +100,11 @@ export default {
 <style>
   #main-form-register {
     padding: 20px;
+  }
+  .alert h3 {
+    font-size: 24px;
+  }
+  .alert {
+    border-left: 4px red solid;
   }
 </style>
