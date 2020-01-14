3cfaa7457ac3ad956b6d442e298a6a6e2186ec7d
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-13 23:46:32 -0300

    add new features and menu mobile

--- ccm-web/src/App.vue
+++ ccm-web/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <opnmenu/>
   </div>
 </template>
 
 <script>
 import sidebar from './components/Sidebar';
 import auth from './components/firebaseinit';
+import opnmenu from './components/menu';
 
 export default {
   name: 'App',
   components: {
     sidebar,
+    opnmenu,
   },
   data() {
     return {
@@ -88,5 +91,46 @@ export default {
   }
   .modal#modal1 {
     height: 410px;
+  }
+  @media only screen and (max-width: 600px) {
+    .main-page {
+      transform: translateX(0);
+      width: 100%;
+    }
+    .dash-block {
+      width: 100%;
+      border-radius: 0;
+    }
+    .dash-block h2 {
+      font-size: 12px;
+    }
+    #quadros-row {
+      margin: 0;
+    }
+    .quadros.col {
+      padding: 0;
+    }
+    #01 {
+      width: 20px;
+      background-color: white;
+      height: 1px;
+    }
+    #02 {
+      width: 20px;
+      background-color: white;
+      height: 1px;
+    }
+    #03 {
+      width: 20px;
+      background-color: white;
+      height: 1px;
+    }
+    #open-menu {
+      background-color: #024873;
+      position: absolute;
+      height: 45px;
+      width: 45px;
+      top: 0;
+    }
   }
 </style>
