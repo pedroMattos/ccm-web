0cffdb3f43aed8134bbf86a207d7ed4d9fece029
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-13 23:46:44 -0300

    add open menu mobiel

---
+++ ccm-web/src/components/menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click.prevent="open" id="open-menu">
+    <div id="01"></div>
+    <div id="02"></div>
+    <div id="03"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'open-menu',
+  methods: {
+    open() {
+      document.getElementById('main-sidebar')
+        .setAttribute('style', 'transform:translateY(0);transition-duration:.5s;z-index:99');
+    },
+  },
+};
+</script>
