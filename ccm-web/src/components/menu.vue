0958e8617f4e37a25795c5fb7221304df7d1101d
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-14 22:08:30 -0300

    add animation to menu no mobile screens

--- ccm-web/src/components/menu.vue
+++ ccm-web/src/components/menu.vue
@@ -1,8 +1,7 @@
 <template>
-  <div @click.prevent="open" id="open-menu">
-    <div id="01"></div>
-    <div id="02"></div>
-    <div id="03"></div>
+  <div @click.prevent="open" opened="false" ref="controller_menu" id="open-menu">
+    <i id="open" class="material-icons small white-text">list</i>
+    <i id="close" style="display:none" class="material-icons small white-text">close</i>
   </div>
 </template>
 
@@ -11,8 +10,26 @@ export default {
   name: 'open-menu',
   methods: {
     open() {
-      document.getElementById('main-sidebar')
-        .setAttribute('style', 'transform:translateY(0);transition-duration:.5s;z-index:99');
+      // eslint-disable-next-line eqeqeq
+      if (document.getElementById('open-menu').getAttribute('opened') == 'true') {
+        document.getElementById('main-sidebar')
+          .removeAttribute('style');
+        document.getElementById('open')
+          .setAttribute('style', 'display:block');
+        document.getElementById('close')
+          .setAttribute('style', 'display:none');
+        this.$refs.controller_menu.removeAttribute('style');
+        this.$refs.controller_menu.setAttribute('opened', 'false');
+      } else {
+        document.getElementById('main-sidebar')
+          .setAttribute('style', 'transform:translateY(0);transition-duration:.5s;z-index:99');
+        document.getElementById('open')
+          .setAttribute('style', 'display:none');
+        document.getElementById('close')
+          .setAttribute('style', 'display:block');
+        this.$refs.controller_menu.setAttribute('style', 'z-index:999');
+        this.$refs.controller_menu.setAttribute('opened', 'true');
+      }
     },
   },
 };
