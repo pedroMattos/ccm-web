e3a2de3d8ca965dfdce92f38ee364f6466a4053f
PedroM <pedro@dnadevendas.com.br>
2020-01-16 16:55:37 -0300

    add filter

---
+++ ccm-web/src/components/filtro-ver-todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row" id="main-see-cats">
+    <div v-for="item in names" :key="item.class" id="loop">
+      <div :class="[item.class]"><p class="white-text">{{ item.name }}</p></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'filtro',
+  data() {
+    return {
+      names: [
+        { name: 'Em manutenção', class: 'em-manutencao' },
+        { name: 'Estado crítico', class: 'estado-critico' },
+        { name: 'Inutilizado', class: 'inutilizado' },
+        { name: 'Sem dono', class: 'sem-dono' },
+        { name: 'Em manutenção', class: 'em-manutencao' },
+      ],
+    };
+  },
+};
+</script>
