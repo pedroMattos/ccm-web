9934ab789c1cbf3256e7e66f718c50bca2b1aec1
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-17 00:28:37 -0300

    add styles

--- ccm-web/src/components/filtro-ver-todos.vue
+++ ccm-web/src/components/filtro-ver-todos.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="row" id="main-see-cats">
-    <div v-for="item in names" :key="item.class" id="loop">
-      <div :class="[item.class]"><p class="white-text">{{ item.name }}</p></div>
+  <div class="row container" id="main-see-cats">
+    <div v-for="item in names" :class="[item.class, 'center']" :key="item.class" id="loop">
+      <router-link
+      :to="{ name: 'filter-entry', params: { nome: item.class } }">
+      <div><p>{{ item.name }}</p></div></router-link>
     </div>
   </div>
 </template>
@@ -22,3 +24,38 @@ export default {
   },
 };
 </script>
+
+<style>
+  #main-see-cats {
+    display: inline-flex;
+    font-weight: 600;
+  }
+  #main-see-cats > div {
+    width: 150px;
+    margin: 0 10px;
+    padding: 5px;
+    border-radius: 2px;
+    height: 36px;
+    cursor: pointer;
+  }
+  #main-see-cats > div p {
+    margin: 0;
+  }
+  #main-see-cats .inutilizado a {
+    color: red;
+  }
+  #main-see-cats a {
+    color: white;
+  }
+  @media only screen and (max-width: 799px) {
+    #main-see-cats {
+      display: block;
+      font-weight: 600;
+      margin: auto;
+    }
+    #main-see-cats > div {
+      width: 100%;
+      margin: 0;
+    }
+  }
+</style>
