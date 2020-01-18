9c53952d851eadc7d05f6ef347944f4536141f25
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-18 12:01:03 -0300

    qr code scanner added successfully

--- ccm-web/src/components/pages/QRCodeReader.vue
+++ ccm-web/src/components/pages/QRCodeReader.vue
@@ -7,14 +7,17 @@
         <h2 class="grey-text lighten-2"
         id="sub-t-page">Funcionalidade em testes <br> <p>Qr Code Scanner</p></h2>
       </blockquote>
-      <video id="preview"></video>
+      <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+      <!-- <qrcode-drop-zone></qrcode-drop-zone> -->
+      <qrcode-capture></qrcode-capture>
       <p>{{ content }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import QrScanner from 'instascan';
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader';
+// import QrScanner from 'instascan';
 import back from '../svg-components/back';
 import sidebar from '../Sidebar';
 
@@ -23,34 +26,19 @@ export default {
   components: {
     sidebar,
     back,
+    QrcodeStream,
+    QrcodeDropZone,
+    QrcodeCapture,
   },
   data() {
     return {
       content: null,
     };
   },
-  created() {
-    const context = this;
-    // instancia a classe
-    const scan = new QrScanner.Scanner(
-      {
-        video: document.getElementById('preview'),
-      },
-    );
-    // listenner do scan
-    scan.addListener('scan', (content) => {
-      context.content = content;
-    });
-    // verifica se existe camera
-    QrScanner.Camera.getCameras().then((device) => {
-      // se tiver mais de 0 cameras
-      if (device.length > 0) {
-        // inicia a primeira
-        scan.start(device[0]);
-      } else {
-        context.content = 'Nenhuma câmera localizada no dispositivo!';
-      }
-    });
+  methods: {
+    onDecode(decodedString) {
+      this.content = decodedString;
+    },
   },
 };
 </script>
