9f3767c2d12168cbad1f3637ca549499aa064349
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-18 13:25:57 -0300

    add component patch

---
+++ ccm-web/src/components/patchN.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="main-block-pt">
+    <div class="col m6 s12">
+      <p>Patch Notes <span id="news">New atts</span></p>
+      <p>Version application: <span id="ver">1.0</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bPatch',
+};
+</script>
+
+<style>
+
+</style>
