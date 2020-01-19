c4397a6f8d221d35ba8a00f76bc3e2677ef60336
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-01-19 01:54:55 -0300

    add qr sacnner

--- ccm-web/src/components/pages/QRCodeReader.vue
+++ ccm-web/src/components/pages/QRCodeReader.vue
@@ -7,17 +7,20 @@
         <h2 class="grey-text lighten-2"
         id="sub-t-page">Funcionalidade em testes <br> <p>Qr Code Scanner</p></h2>
       </blockquote>
-      <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+      <video id="preview"></video>
       <!-- <qrcode-drop-zone></qrcode-drop-zone> -->
-      <!-- <qrcode-capture></qrcode-capture> -->
+      <QrcodeCapture @decode="onDecode"></QrcodeCapture>
       <p>{{ content }}</p>
+      <p>{{ errorMessage }}</p>
     </div>
   </div>
 </template>
 
 <script>
+// import adapter from 'webrtc-adapter';
+// import jsQR from 'jsqr';
 import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader';
-// import QrScanner from 'instascan';
+import QrScanner from 'instascan';
 import back from '../svg-components/back';
 import sidebar from '../Sidebar';
 
@@ -30,23 +33,47 @@ export default {
     QrcodeDropZone,
     QrcodeCapture,
   },
+
   data() {
     return {
-      content: null,
+      content: '',
+      errorMessage: '',
     };
   },
+  mounted() {
+    const context = this;
+    function qrscan() {
+      const mediaStream = new MediaStream();
+      const video = document.getElementById('preview');
+      video.srcObject = mediaStream;
+      // eslint-disable-next-line prefer-const
+      let scanner = new QrScanner.Scanner({ video });
+      // console.log(scanner);
+      scanner.addListener('scan', (content) => {
+        context.content = content;
+      });
+      QrScanner.Camera.getCameras().then((cameras) => {
+        if (cameras.length > 0) {
+          scanner.start(cameras[1]);
+        } else {
+          context.errorMessage = 'Nenhuma câmera encontrada';
+        }
+      }).catch((e) => {
+        context.errorMessage = `Ocorreu um erro: ${e}`;
+      });
+    }
+    qrscan();
+  },
   methods: {
-    onDecode(decodedString) {
-      this.content = decodedString;
+    onDecode(content) {
+      this.content = content;
     },
   },
 };
 </script>
 
 <style>
-  #preview {
-    border: solid 1px grey;
-    border-radius: 8px;
-    background-color: azure;
+  video {
+    max-height: 255px;
   }
 </style>
