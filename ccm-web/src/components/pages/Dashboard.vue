1bf9781306cba79f08dbe92eef25a5850d401c23
PedroM <pedro@dnadevendas.com.br>
2020-01-21 16:39:20 -0300

    add link to patch notes

--- ccm-web/src/components/pages/Dashboard.vue
+++ ccm-web/src/components/pages/Dashboard.vue
@@ -3,6 +3,8 @@
     <sidebar/>
     <div class="col m9 s12 main-page" id="main-dashboard">
       <h1>CCM - Web Version <span>Beta</span> <iconVer/></h1>
+      <router-link :to="{ name: 'PatchNotes' }">
+        <p id="to-release">Leia as notas de atualização <span>Novo!</span></p></router-link>
       <blockquote class="tip">
         <h2 class="grey-text lighten-2"
         id="sub-t-page">Bem vindo(a)!</h2>
@@ -72,12 +74,45 @@ export default {
   #main-dashboard h1{
     font-size: 32px;
   }
-    #main-dashboard h1 span{
-      background-color: blue;
-      padding: 2px;
-      border-radius: 5px;
-      color: white;
-      font-size: 12px;
+  #main-dashboard h1 span{
+    background-color: blue;
+    padding: 2px;
+    border-radius: 5px;
+    color: white;
+    font-size: 12px;
+  }
+  a #to-release{
+    font-weight: 600;
+    color: rgb(82, 82, 82);
+    position: relative;
+    /* overflow: hidden; */
+    width: max-content;
+  }
+  a #to-release::after {
+    content: '';
+    position: absolute;
+    bottom: -4px;
+    height: 1px;
+    background-color: rgba(82, 82, 82, 0.616);
+    width: 0;
+    transition-duration: .5s;
+    left: 0;
+  }
+  a:hover #to-release::after {
+    content: '';
+    position: absolute;
+    bottom: -4px;
+    height: 1px;
+    background-color: rgba(82, 82, 82, 0.616);
+    width: 100%;
+    transition-duration: .5s;
+  }
+  #to-release span {
+    font-size: 12px;
+    background-color: rgb(64, 184, 64);
+    color: white;
+    padding: 2px;
+    border-radius: 4px;
   }
   .quadros {
     position: relative;
